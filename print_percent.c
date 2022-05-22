825917b6bdf692ed7b0839be81d5e335205c83b8
mazi-kunle <olakunlemichael644@gmail.com>
2022-05-22 08:01:52 -0700

    edit

--- print_percent.c
+++ print_percent.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * print_pecent- a function that prints percentage.
- * @aeg: parameter.
+ * print_percent- a function that prints percentage.
+ * @arg: parameter.
  *
- * Return:  count.
+ * Return: count.
 */
 
-int print_percent(__attribute__((unused))va_list *arg)
+int print_percent(__attribute__((unused)) va_list * arg)
 {
 	_putchar('%');
 	return (1);
