1f3862b361c9e8c090e22d83c4726a070225d704
Christabel111 <98888294+Christabel111@users.noreply.github.com>
2022-05-22 12:41:38 +0100

    Update special_functions.c

--- special_functions.c
+++ special_functions.c
@@ -34,56 +34,6 @@ int print_S(va_list *arg)
 		i++;
 	}
 	return (count);
-	/*
-	for (i = 0; s[i]; i++)
-	{
-		if ((s[i] < 0 || s[i] < 32 || s[i] >= 127))
-		{
-			_putchar('\\');
-			count++;
-			_putchar('x');
-			count++;
-			_putchar(0 + '0');
-			count++;
-			print_base(s[i], 16, 0, &count);
-		}
-		else
-		{
-			_putchar(s[i]);
-			count++;
-		}
-	}
-	return (count); */
-}
-
-/**
- * print_rev- a function.
- * @arg: parameter.
- * Return: count.
-*/
-
-int print_rev(va_list *arg)
-{
-	char *temp, *end;
-	int i, count;
-
-	temp = va_arg(*arg, char *);
-	end = temp;
-	count = 0;
-	while (*end)
-	{
-		count++;
-		end++;
-	}
-	if (temp != NULL)
-	{
-		for (i = count - 1; i >= 0; i--)
-		{
-			_putchar(temp[i]);
-		}
-		_putchar('\0');
-	}
-	return (count);
 }
 
 /**
@@ -124,13 +74,6 @@ int print_octal(va_list *arg)
 		return (1);
 	}
 	return (count);
-	/*unsigned int d;
-	int count;
-
-	d = va_arg(*arg, unsigned int);
-	count = 0;
-	print_base(d, 8, 2, &count);
-	return (count);*/
 }
 	
 /**
@@ -183,14 +126,7 @@ int print_HEXnhex(unsigned int n, unsigned int c)
 int print_hex(va_list x)
 {
 	return (print_HEXnhex(va_arg(x, unsigned int), 0));
-	/*
-	unsigned int d;
-	int count;
-
-	d = va_arg(*arg, unsigned int);
-	count = 0;
-	print_base(d, 16, 1, &count);
-	return (count); */
+	
 }
 
 /**
@@ -202,12 +138,5 @@ int print_hex(va_list x)
 int print_HEX(va_list X)
 {
 	return (print_HEXnhex(va_arg(X, unsigned int), 1));
-	/*
-	unsigned int d;
-	int count;
-
-	d = va_arg(*arg, unsigned int);
-	count = 0;
-	print_base(d, 16, 0, &count);
-	return (count); */
+	
 }
