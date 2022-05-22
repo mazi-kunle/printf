04aa66945a19060c8f6f893fc004fa92ad921f12
Christabel111 <98888294+Christabel111@users.noreply.github.com>
2022-05-22 12:15:47 +0100

    Update main.h

--- main.h
+++ main.h
@@ -13,8 +13,9 @@ int print_bin(va_list *arg);
 int print_S(va_list *arg);
 int print_rev(va_list *arg);
 int print_octal(va_list *arg);
-int print_hex(va_list *arg);
-int print_HEX(va_list *arg);
+int print_HEXnhex(unsigned int n, unsigned int c);
+int print_hex(va_list x);
+int print_HEX(va_list X);
 
 /*helper functions*/
 void print_base(unsigned int num, int base, int det, int *count);
